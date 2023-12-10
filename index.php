f9196dd627c07d1b7cc6723fa728f04382c36082
YDST <yudis10@users.noreply.github.com>
2023-12-10 09:16:50 +0700

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+<!-- index.php -->
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script type="module" src="react-build/react.js"></script>
+	</head>
+	<body>
+		<?="Render some text with PHP!";?>
+
+		<div data-name="detik-vertical" data-target="block/cbpopuler">
+		  <div><span>placeholder</span></div>
+	  </div>
+  	<div data-target="nonheadline" name="newsfeed1">
+  		<div><span>placeholder</span></div>
+  		<div><span>placeholder</span></div>
+  	</div>
+	</body>
+</html>
